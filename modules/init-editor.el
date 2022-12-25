3d1fd7b6edfa89ffb4a76d80bf1660b8c4d27871
libvirgo <sakurapetgirl@live.com>
2022-12-25 20:37:09 +0800

    add rime as default chinese input method.

--- modules/init-editor.el
+++ modules/init-editor.el
@@ -24,4 +24,26 @@
         (prog-mode . hl-indent-scope-mode)
         (after-load-theme . hl-indent-scope--auto-color-calc)))
 
+(use-package rime
+  :straight (rime :type git
+                  :host github
+                  :repo "DogLooksGood/emacs-rime"
+                  :files ("*.el" "Makefile" "lib.c"))
+  :custom
+  (default-input-method "rime")
+  (rime-emacs-module-header-root "/opt/homebrew/Cellar/emacs-plus@29/29.0.60/include")
+  (rime-librime-root (expand-file-name "librime/dist" prelude-local-dir))
+  (rime-show-candidate 'posframe)
+  (rime-user-data-dir "~/Library/Rime")
+  (rime-posframe-style 'vertical)
+  :bind (:map rime-mode-map ("M-j" . rime-force-enable))
+  :config
+  (setq rime-disable-predicates '(meow-normal-mode-p
+				  meow-motion-mode-p
+				  meow-keypad-mode-p
+				  rime-predicate-prog-in-code-p
+				  rime-predicate-after-alphabet-char-p))
+  (setq rime-inline-predicates '(rime-predicate-space-after-cc-p
+				                 rime-predicate-current-uppercase-letter-p)))
+
 (provide 'init-editor)
