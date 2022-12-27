b1cd1194ebf4af3ae92fe8aadebbce35bc635b4b
libvirgo <sakurapetgirl@live.com>
2022-12-27 21:46:42 +0800

    assimilate: use-package and dependencies.

--- init.el
+++ init.el
@@ -1,4 +1,12 @@
-(add-to-list 'load-path (expand-file-name ".local/lib/borg" user-emacs-directory))
-(require 'borg)
-(borg-initialize)
+(eval-and-compile ; `borg'
+  (add-to-list 'load-path (expand-file-name ".local/lib/borg" user-emacs-directory))
+  (require 'borg)
+  (borg-initialize))
+
 (setq epkg-repository (expand-file-name ".local/lib/epkgs" user-emacs-directory))
+
+(eval-and-compile ; `use-package'
+  (require  'use-package)
+  (setq use-package-verbose t)
+  (setq-default use-package-always-defer t))
+
