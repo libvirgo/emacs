0026a94ce5af03c0d7813123339a027f8b0d4093
libvirgo <sakurapetgirl@live.com>
2022-12-27 21:31:10 +0800

    assimilate: epkg and dependencies.

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
 (add-to-list 'load-path (expand-file-name ".local/lib/borg" user-emacs-directory))
 (require 'borg)
 (borg-initialize)
+(setq epkg-repository (expand-file-name ".local/lib/epkgs" user-emacs-directory))
