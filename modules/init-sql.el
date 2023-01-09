c55b333df57f455bc2b3f716a29af37e444b6bfe
libvirgo <sakurapetgirl@live.com>
2023-01-09 20:58:33 +0800

    change ejc column width limit.

--- modules/init-sql.el
+++ modules/init-sql.el
@@ -2,6 +2,8 @@
 
 (use-package ejc-sql
   :hook ((sql-mode . ejc-sql-mode)
+		 (ejc-sql-connected . (lambda ()
+								(ejc-set-column-width-limit 255)))
 		 (ejc-sql-minor-mode . (lambda ()
 								(require 'ejc-company)
 								(setq-local completion-at-point-functions
@@ -9,7 +11,6 @@
 												   (cape-company-to-capf #'ejc-company-backend)
 												   #'cape-dabbrev
 												   ))))))
-  
   :config
   (setq clomacs-httpd-default-port 8090) ; Use a port other than 8080.
   (setq ejc-temp-editor-file-path clytie-sql-dir)
