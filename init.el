900af3cf308adaef43f50b4428092d4ad8306d81
libvirgo <sakurapetgirl@live.com>
2023-01-11 13:25:57 +0800

    change default url directory.

--- init.el
+++ init.el
@@ -10,11 +10,14 @@
       `((".*" ,clytie-tmp-dir t)))
 (setq auto-save-list-file-prefix
       clytie-tmp-dir)
-(setq transient-levels-file (expand-file-name "transient/levels.el" clytie-cache-dir))
-(setq transient-values-file (expand-file-name "transient/values.el" clytie-cache-dir))
-(setq transient-history-file (expand-file-name "transient/history.el" clytie-cache-dir))
-(setq bookmark-default-file (expand-file-name "bookmarks" clytie-cache-dir))
-(setq make-backup-files nil)
+(setq transient-levels-file (expand-file-name "transient/levels.el" clytie-cache-dir)
+	  transient-values-file (expand-file-name "transient/values.el" clytie-cache-dir)
+	  transient-history-file (expand-file-name "transient/history.el" clytie-cache-dir)
+	  bookmark-default-file (expand-file-name "bookmarks" clytie-cache-dir)
+	  make-backup-files nil
+	  url-configuration-directory (expand-file-name "url" clytie-local-dir)
+	  url-cookie-file (expand-file-name "cookies" url-configuration-directory)
+	  url-cache-directory (expand-file-name "cache" url-configuration-directory))
 
 (setq large-file-warning-threshold 100000000)
 (setq load-prefer-newer t)
