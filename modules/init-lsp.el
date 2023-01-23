bc82c75d7c55e2130d4b9cb6ad29d87cd84c1e8f
libvirgo <sakurapetgirl@live.com>
2023-01-24 00:33:10 +0800

    add embark keymap for lsp-ui-mode.

--- modules/init-lsp.el
+++ modules/init-lsp.el
@@ -12,14 +12,18 @@
   (lsp-signature-doc-lines 0)
   (lsp-session-file (expand-file-name "lsp-session-v1" clytie-cache-dir))
   :hook
-  ((lsp-mode . (lambda () (yas-minor-mode-on))))
+  ((lsp-mode . (lambda () (yas-minor-mode-on)))
+   (lsp-completion-mode . my/lsp-mode-setup-completion))
   :init
   (setq lsp-keymap-prefix "C-c l")
   (defun my/lsp-mode-setup-completion ()
     (setf (alist-get 'styles (alist-get 'lsp-capf completion-category-defaults))
           '(orderless))) ;; Configure orderless
-  :hook
-  (lsp-completion-mode . my/lsp-mode-setup-completion))
+  (with-eval-after-load 'embark
+      (push 'embark--allow-edit
+            (alist-get 'lsp-rename embark-target-injection-hooks)))
+  (remove-hook 'flymake-diagnostic-functions 'flymake-proc-legacy-flymake)
+  )
 
 (use-package lsp-ui
   :custom
@@ -28,6 +32,12 @@
   (lsp-ui-doc-include-signature t)
   ;; (lsp-ui-doc-use-webkit t)
   (lsp-ui-doc-position 'at-point)
-  )
+  :hook
+  ((lsp-ui-mode . (lambda ()
+					(with-eval-after-load 'embark
+					  (make-local-variable 'embark-identifier-map)
+					  (setq embark-identifier-map (copy-tree embark-identifier-map))
+					  (keymap-set embark-identifier-map "M-." #'lsp-ui-peek-find-definitions)
+					  (keymap-set embark-identifier-map "M-?" #'lsp-ui-peek-find-references))))))
 
 (provide 'init-lsp)
